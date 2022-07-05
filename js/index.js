fe6a91fb320d1a801c238bf1065c74f085cbdd24
SeanKeane6 <108425807+Seankeane1@users.noreply.github.com>
2022-07-05 16:25:23 -0400

    Fixed css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-const finalScore = document.querySelector(".score-value")
+// Build an object and array model with answers and assigned values
+
+const questions = {
+
+};
+
+// add event listeners to all the options and an if statememt to determine the score value
+
+// Make a function that both toggles scorecard visibility and displays the final score 
+
+console.log('hello')
