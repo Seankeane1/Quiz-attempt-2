b29eca3d5cf02a711dc1c7f2d204e88c30fe177b
SeanKeane6 <108425807+Seankeane1@users.noreply.github.com>
2022-07-10 13:17:28 -0400

    I don't remember how DOMSelectors worl

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
+DOMSelectors (
+    const quizContainer = document.querySelectorAll(".container")
+);
+
 // Build an object and array model with answers and assigned values
 
 const questions = {
 
 };
+
+console.log(DOMSelectors.quizContainer);
 
 // add event listeners to all the options and an if statememt to determine the score value
 
